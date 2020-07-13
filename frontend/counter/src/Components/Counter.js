75442a979713f757d766ca793637b4b76610aff4
RahulNTrivedi <rahult0705@gmail.com>
2020-07-13 14:40:40 -0400

    refresh page

--- frontend/counter/src/Components/Counter.js
+++ frontend/counter/src/Components/Counter.js
@@ -12,6 +12,7 @@ class Counter extends React.Component {
         daysSince: props.daysSince
       })
     });
+    window.location.reload(false);
   }
   deleteCounter(props) {
     fetch("http://127.0.0.1:5000/delete", {
@@ -22,6 +23,7 @@ class Counter extends React.Component {
         daysSince: props.daysSince
       })
     });
+    window.location.reload(false);
   }
   render() {
     return(
